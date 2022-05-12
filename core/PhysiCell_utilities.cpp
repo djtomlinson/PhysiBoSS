ffe94d9511e499383acf084cc5bbcd0fa14ec5b0
Vincent Noël <contact@vincent-noel.fr>
2022-05-12 19:24:06 +0200

    Change timestep noise in PhysiBoSS to now follow a log normal distribution

--- core/PhysiCell_utilities.cpp
+++ core/PhysiCell_utilities.cpp
@@ -196,6 +196,11 @@ double LogNormalRandom( double mean, double standard_deviation )
 	return exp(NormalRandom(log(mean), standard_deviation));
 }
 
+double LogNormalRandom( double mean, double standard_deviation )
+{
+	return exp(NormalRandom(log(mean), standard_deviation));
+}
+
 std::vector<double> UniformOnUnitSphere( void )
 {
 	std::vector<double> output = {0,0,0}; 
